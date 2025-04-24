3c7e2e47adb753bc4de367051cfed4a07b7bcee8
Abhishek Kumar <abhishekcusat21@gmail.com>
2025-04-25 00:17:04 +0530

    basic struture

---
+++ Circuit-Breaker/src/main/java/com/spring/Circuit_Breaker/CircuitBreakerRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Circuit_Breaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerRateServiceApplication.class, args);
+	}
+
+}
---
+++ Circuit-Breaker/src/test/java/com/spring/Circuit_Breaker/CircuitBreakerRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Circuit_Breaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
