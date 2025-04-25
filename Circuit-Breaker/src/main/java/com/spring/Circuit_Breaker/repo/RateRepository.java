fe047afe1e859c96941fd8d5dfd41a7dba182489
Abhishek Kumar <abhishekcusat21@gmail.com>
2025-04-25 17:38:34 +0530

    add repo

--- Circuit-Breaker/src/main/java/com/spring/Circuit_Breaker/repo/RateRepository.java
+++ Circuit-Breaker/src/main/java/com/spring/Circuit_Breaker/repo/RateRepository.java
@@ -3,6 +3,10 @@ package com.spring.Circuit_Breaker.repo;
 import com.spring.Circuit_Breaker.entity.Rate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface RateRepository extends JpaRepository<Rate, Integer> {
+
+    Optional<Rate> findByCurrency(String type);
 }
